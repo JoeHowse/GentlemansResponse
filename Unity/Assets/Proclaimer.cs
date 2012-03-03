193b42b43fbbdf6490394d55c5446e51f1bd4ddd
Joe <josephhowse@nummist.com>
2012-03-03 01:37:55 -0400

    Fonts, Proclaimer, Proclaimation

---
+++ Unity/Assets/Proclaimer.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class Proclaimer : MonoBehaviour {
+	public Transform proclamationPrefab;
+	
+	public void Proclaim(string text, float duration) {
+		Transform proclamation = Instantiate(proclamationPrefab) as Transform;
+		foreach(GUIText guiText in proclamation.GetComponentsInChildren<GUIText>()) {
+			if(guiText.name.Equals("Content")) {
+				guiText.text = text;
+			}
+		}
+		Destroy(proclamation.gameObject, duration);
+	}
+}
